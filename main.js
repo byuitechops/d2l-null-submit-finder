0c3b42d10e4e21571a3249eabbf06839cd420af6
Benjamin Earl <ear15002@byui.edu>
2018-02-13 14:18:05 -0700

    prototype finished

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
+// /* eslint no-unused-vars:0 no-console:0 */ 
+var $
+
+// Dependencies
 const puppeteer = require('puppeteer')
 const auth = require('./auth.json') // [{username:"",password:""}]
-const qs = require('querystring')
+const Path = require('path')
+const fs = require('fs')
+const moment = require('moment')
 
+// Temporary Globals
 const subdomain = 'pathway'
-const ou = '106509'
-const quizes = ['492840']
+const ous = ['106509']
+const quizzes = ['492840']
+const screenshotFolder = Path.join(__dirname,'shots')
+const testDataFile = Path.join(__dirname,'data.json')
+
+// Selectors
 const username = '[name=userName]'
 const password = '[name=password]'
 const bigButton = 'button[primary]'
 const quizDetailsTable = 'table[role=presentation]'
+const AttemptHeader = 'h2.vui-heading-3'
 
-var pici = 1
+function createTestData (ou,quizId,userId){
+    var date = new Date()
+    return {
+        ou: ou,
+        quizId:quizId,
+        date:date,
+        userId:userId,
+        id: Math.floor(date.getTime()/1000).toString(36).slice(-4),
+        pics: []
+    }
+}
 
-
-async function login(page) {
+async function login(page,userId) {
+    await page.deleteCookie(...await page.cookies())
     await page.goto(`https://${subdomain}.brightspace.com/d2l/login?noredirect=true`)
-    await page.type(username, auth[0].username)
-    await page.type(password, auth[0].password)
+    await page.type(username, auth[userId].username)
+    await page.type(password, auth[userId].password)
     await Promise.all([
         page.waitForNavigation(),
         page.click(bigButton)
@@ -29,27 +51,46 @@ async function main() {
     })
     const page = await browser.newPage()
     await page.setViewport({width:1000,height:1000})
-    await login(page)
-    await takeQuiz(page, '106509', '492840')
-    // await browser.close()
+    
+    // For each user
+    for(var userId = 0; userId < auth.length; userId++){
+        await login(page,userId)
+        // For each course
+        for(var oui = 0; oui < ous.length; oui++){
+            // For each quiz
+            for(var quizi = 0; quizi < quizzes.length; quizi++){
+                // Setup test
+                var testData = createTestData(ous[oui],quizzes[quizi],userId)
+                // DO IT!!
+                await takeQuiz(page,testData)
+                // Write out results
+                appendTestData(testData)
+            }
+        }
+    }
+
+    await browser.close()
 }
 
-async function takePicture(name){
-    var fileName = `${pici.id}_${name}`
-    await pici.page.screenshot({
-        path:'shots/'+fileName+'.png',
-        fullPage:true,
-    })
-}
 
-async function takeQuiz(page, ou, quiz) {
-    var date = new Date()
-    pici = {page:page,ou:ou,quiz:quiz,date:date,user:auth[0].username,id:(Math.floor(date.getTime()/1000)).toString(36).slice(-4)}
-
-    const quizURL = `https://${subdomain}.brightspace.com/d2l/lms/quizzing/user/quiz_summary.d2l?qi=${quiz}&ou=${ou}`
+async function takeQuiz(page, testData) {
+    async function takePicture(name){
+        var filename = `${testData.id}_${moment(testData.date).format('YYYY-D-M--h;mm;ssa')}_${testData.ou}_${testData.quizId}_${testData.userId}_${name}.png`
+        var path = Path.join(screenshotFolder,filename)
+        testData.pics.push(path)
+        await page.screenshot({
+            path:path,
+            fullPage:true,
+        })
+    }
+    // Going to the quiz
+    const quizURL = `https://${subdomain}.brightspace.com/d2l/lms/quizzing/user/quiz_summary.d2l?qi=${testData.quizId}&ou=${testData.ou}`
     await page.goto(quizURL)
+    // Record what it looks like
     await takePicture('before')
-    pici.beforeAttemptNumber = await numCompleted(page)
+    testData.beforeAttemptNumber = await numCompleted(page)
+    
+    // Start the quiz
     await Promise.all([
         page.waitForNavigation(),
         page.click(bigButton)
@@ -65,37 +106,32 @@ async function takeQuiz(page, ou, quiz) {
             $f.find('textarea').val('Haha here is my answer')
         })
     })
-    
     await takePicture('answered')
+    
     // Click submit
     frame = (await Promise.all([
         waitForFrame(page,'quiz_confirm_submit'),
         frame.$eval(bigButton, b => $(b).next().click(),bigButton)
     ]))[0]
-    
     // we waited for the frame to change, now we just need to wait for the button to pop up
     await frame.waitFor(bigButton)
-    
     await takePicture('confirm')
+    
     // And click submit again because it is d2l
     frame = (await Promise.all([
         waitForFrame(page,'quiz_submissions_attempt'),
         frame.$eval(bigButton, b => b.click(),bigButton)
     ]))[0]
 
-    // wait for the fram to actually load
-    await frame.waitFor('h2.vui-heading-3')
-    pici.afterAttemptNumber = await frame.evaluate(() => $('h2:contains("Attempt")').text().match(/\d+/)[0])
-
+    // wait for the frame to actually load
+    await frame.waitFor(AttemptHeader)
+    testData.afterAttemptNumber = await frame.evaluate(() => $('h2:contains("Attempt")').text().match(/\d+/)[0])
     await takePicture('after')
 
-    await page.goto(`https://pathway.brightspace.com/d2l/lms/quizzing/user/quizzes_list.d2l?ou=${ou}`)
-    
-    pici.listAttemptNumber = await page.evaluate(quizId => $(`[onclick*="${quizId}"]`).closest('td').next().text().match(/\d+/)[0],quiz)
-
+    // Go to the quizzes list to see if the attempt showed up
+    await page.goto(`https://pathway.brightspace.com/d2l/lms/quizzing/user/quizzes_list.d2l?ou=${testData.ou}`)
+    testData.listAttemptNumber = await page.evaluate(quizId => $(`[onclick*="${quizId}"]`).closest('td').next().text().match(/\d+/)[0],testData.quizId)
     await takePicture('list')
-
-    console.log(pici)
 }
 
 async function waitForFrame(page,frameName){
@@ -118,12 +154,21 @@ async function numCompleted(page) {
     }
 }
 
-function dumpFrameTree(frame, indent) {
-    indent = indent || ''
-    console.log(indent + frame.url());
-    for (let child of frame.childFrames()){
-        dumpFrameTree(child, indent + '  ');
+function appendTestData(testData){
+    var data = []
+    if(fs.existsSync(testDataFile)){
+        var stringed = fs.readFileSync(testDataFile,'utf-8')
+        data = JSON.parse(stringed)
     }
+    data.push(testData)
+    fs.writeFileSync(testDataFile,JSON.stringify(data))
 }
+// function dumpFrameTree(frame, indent) {
+//     indent = indent || ''
+//     console.log(indent + frame.url());
+//     for (let child of frame.childFrames()){
+//         dumpFrameTree(child, indent + '  ');
+//     }
+// }
 
 main()
